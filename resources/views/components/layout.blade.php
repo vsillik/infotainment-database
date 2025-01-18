84031d24eea8fe456b1e18f88f3358913513f458
Vojtěch Sillik <vojtech.sillik@gmail.com>
2025-01-18 17:58:10 +0100

    Improved navigation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,20 +1,31 @@
-@php use App\Models\InfotainmentManufacturer;use App\Models\SerializerManufacturer; @endphp
+@php
+    use App\Models\Infotainment;
+    use App\Models\InfotainmentManufacturer;
+    use App\Models\SerializerManufacturer;
+    use App\Models\User;
+@endphp
+
 <x-base-layout>
     <header class="navbar bg-dark flex-md-nowrap p-1 shadow" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">{{ config('app.name') }}</a>
+            <a class="navbar-brand col-md-3 col-lg-2 me-0 pe-3 fs-6 text-white" href="#">{{ config('app.name') }}</a>
 
             <div class="dropdown">
                 <a href="#" class="d-block link-light text-decoration-none dropdown-toggle" id="dropdownUser"
                    data-bs-toggle="dropdown" aria-expanded="false">
                     {{ Auth::user()->name }}
                 </a>
-                <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end text-small" aria-labelledby="dropdownUser">
+                <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end text-small"
+                    aria-labelledby="dropdownUser">
                     <li>
-                        <a href="{{ route('profile.edit') }}" class="dropdown-item link-light">Profile settings</a>
+                        <a href="{{ route('profile.edit') }}" class="dropdown-item link-light">
+                            Profile settings
+                        </a>
                     </li>
                     <li>
-                        <a href="{{ route('profile.password.edit') }}" class="dropdown-item link-light">Change password</a>
+                        <a href="{{ route('profile.password.edit') }}" class="dropdown-item link-light">
+                            Change password
+                        </a>
                     </li>
                     <li>
                         <hr class="dropdown-divider">
@@ -33,7 +44,7 @@
 
     <div class="container-fluid h-100 flex-grow-1">
         <div class="row">
-            <div class="sidebar border border-right col-md-2 col-lg-1 p-0 bg-body-tertiary">
+            <div class="sidebar border border-right col-md-2 col-lg-1 py-0 px-2 bg-body-tertiary">
                 <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu"
                      aria-labelledby="sidebarMenuLabel">
                     <div class="offcanvas-header">
@@ -41,19 +52,41 @@
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
                                 data-bs-target="#sidebarMenu" aria-label="Close"></button>
                     </div>
-                    <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-                        <ul class="nav flex-column">
+                    <div class="offcanvas-body d-md-flex flex-column pb-3 pt-md-3 overflow-y-auto">
+                        <ul class="nav nav-pills flex-column">
                             <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page"
-                                   href="{{ route('index') }}">
+                                <a @class([
+                                       'nav-link',
+                                       'd-flex',
+                                       'align-items-center',
+                                       'gap-2',
+                                       'mt-1',
+                                       'active' => request()->routeIs('index')
+                                   ])
+                                   @if(request()->routeIs('index'))
+                                       aria-current="page"
+                                   @endif
+                                   href="{{ route('index') }}"
+                                >
                                     Home
                                 </a>
                             </li>
 
                             @can('viewAny', InfotainmentManufacturer::class)
                                 <li class="nav-item">
-                                    <a class="nav-link d-flex align-items-center gap-2"
-                                       href="{{ route('infotainment_manufacturers.index') }}">
+                                    <a @class([
+                                           'nav-link',
+                                           'd-flex',
+                                           'align-items-center',
+                                           'gap-2',
+                                           'mt-1',
+                                           'active' => request()->routeIs('infotainment_manufacturers.*')
+                                       ])
+                                       @if(request()->routeIs('infotainment_manufacturers.*'))
+                                           aria-current="page"
+                                       @endif
+                                       href="{{ route('infotainment_manufacturers.index') }}"
+                                    >
                                         Infotainment manufacturers
                                     </a>
                                 </li>
@@ -61,37 +94,65 @@
 
                             @can('viewAny', SerializerManufacturer::class)
                                 <li class="nav-item">
-                                    <a class="nav-link d-flex align-items-center gap-2"
-                                       href="{{ route('serializer_manufacturers.index') }}">
+                                    <a @class([
+                                           'nav-link',
+                                           'd-flex',
+                                           'align-items-center',
+                                           'gap-2',
+                                           'mt-1',
+                                           'active' => request()->routeIs('serializer_manufacturers.*')
+                                       ])
+                                       @if(request()->routeIs('serializer_manufacturers.*'))
+                                           aria-current="page"
+                                       @endif
+                                       href="{{ route('serializer_manufacturers.index') }}"
+                                    >
                                         Serializer manufacturers
                                     </a>
                                 </li>
                             @endcan
 
-                            @can('viewAny', \App\Models\Infotainment::class)
+                            @can('viewAny', Infotainment::class)
                                 <li class="nav-item">
-                                    <a class="nav-link d-flex align-items-center gap-2"
-                                       href="{{ route('infotainments.index') }}">
+                                    <a @class([
+                                           'nav-link',
+                                           'd-flex',
+                                           'align-items-center',
+                                           'gap-2',
+                                           'mt-1',
+                                           'active' => request()->routeIs('infotainments.*')
+                                       ])
+                                       @if(request()->routeIs('infotainments.*'))
+                                           aria-current="page"
+                                       @endif
+                                       href="{{ route('infotainments.index') }}"
+                                    >
                                         Infotainments
                                     </a>
                                 </li>
                             @endcan
 
-                            @can('viewAny', \App\Models\User::class)
-                            <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2"
-                                   href="{{ route('users.index') }}">
-                                    Users
-                                </a>
-                            </li>
+                            @can('viewAny', User::class)
+                                <li class="nav-item">
+                                    <a @class([
+                                           'nav-link',
+                                           'd-flex',
+                                           'align-items-center',
+                                           'gap-2',
+                                           'mt-1',
+                                           'active' => request()->routeIs('users.*')
+                                       ])
+                                       @if(request()->routeIs('users.*'))
+                                           aria-current="page"
+                                       @endif
+                                       href="{{ route('users.index') }}"
+                                    >
+                                        Users
+                                    </a>
+                                </li>
                             @endcan
 
                             <!--
-                            <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2" href="#">
-                                    Preferences
-                                </a>
-                            </li>
                             <li class="nav-item">
                                 <a class="nav-link d-flex align-items-center gap-2" href="#">
                                     Site settings
