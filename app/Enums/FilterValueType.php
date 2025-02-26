05f502c0779a120242da09624c3352563c1ddc70
Vojtěch Sillik <vojtech.sillik@gmail.com>
2025-02-26 22:03:37 +0100

    Fix filter containing '0'

--- app/Enums/FilterValueType.php
+++ app/Enums/FilterValueType.php
@@ -17,7 +17,7 @@ enum FilterValueType: string
     public function validateValueType(mixed $value): bool
     {
         return match ($this) {
-            self::STRING, self::DATE => is_string($value) && ! empty($value),
+            self::STRING, self::DATE => is_string($value) && $value !== '',
             self::YES_NO_CHOICE => is_string($value) && in_array($value, ['yes', 'no', 'any']),
             self::USER_ROLE => $value === 'any' || (is_numeric($value) && UserRole::tryFrom(intval($value)) !== null),
             self::NUMERIC => is_numeric($value),
